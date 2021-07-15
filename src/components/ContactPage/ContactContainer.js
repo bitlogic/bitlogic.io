2f4ac1aebd3feeabf880b9bf43e243a3140d2c9b
Yuliya Sharapa <yuliya.sharapa@bitlogic.io>
2021-07-14 23:04:55 -0300

    add contact form using react hook form

--- src/components/ContactPage/ContactContainer.js
+++ src/components/ContactPage/ContactContainer.js
@@ -1,17 +1,80 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import { getImage, GatsbyImage } from "gatsby-plugin-image";
+import { useForm } from "react-hook-form";
+import "./Contact.scss"
 
 import Layout from "../../components/layout"
 
 import { Seo } from "../index"
 
-const Contact = () => (
-  <Layout>
-    <Seo />
-    <h1>Hi from the Contact page</h1>
-    <p>Welcome to Contact</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const Contact = ({image}) => {
+
+  // const image = getImage(image);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+
+  const onSubmit = data => {
+    console.log(data)
+  }
+
+  console.log({ errors })
+
+  return (
+    <Layout>
+      <Seo />
+      <div className="contact">
+        <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="contactForm"
+          >
+            
+            <input 
+              className="contactForm__input"
+              type="text"
+              placeholder="Nombre"
+              {...register("name", { required: true, maxLength: 50 })}
+            />
+          
+            <input 
+              className="contactForm__input"
+              type="text"
+              placeholder="Apellido"
+              {...register("lastName", { required: true, maxLength: 50 })}
+            />
+
+            <input 
+              className="contactForm__input"
+              type="text"
+              placeholder="Correo"
+              {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
+            />
+
+            <input 
+              className="contactForm__input"
+              type="text"
+              placeholder="Institución"
+              {...register("institution", { required: true, maxLength: 50 })}
+            />      
+
+            <input type="submit" value="Enviar" className="contactForm__inputSubmit" />
+          </form>
+
+          {/* <div>
+            <GatsbyImage 
+              image={image} 
+              // alt={article.title} 
+            />
+          </div> */}
+      </div>
+     
+
+    </Layout>
+    )
+}
 
 export default Contact
