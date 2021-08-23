0d1e9fc45fdfa0a08347bfcb69e270e0b10a92cf
Benjamin Cañas <benjamin.canas@bitlogic.io>
2021-08-23 05:45:12 -0300

    usebitway fix

--- src/hooks/useBitwayPage.js
+++ src/hooks/useBitwayPage.js
@@ -1,32 +1,54 @@
 import { useStaticQuery, graphql } from "gatsby";
+import { useEffect, useState } from "react";
 
 const useBitwayPage = () => {
   const query = useStaticQuery(graphql`
     {
       allStrapiBitwayPage {
         nodes {
-          Section {
-            title
-            Type
+          sections {
             body
             id
-            enabled
-            Image {
-              Caption
-              Image {
+            type
+            galleryImage {
+              caption
+              text
+              id
+              image {
                 childImageSharp {
                   gatsbyImageData(quality: 100, layout: FULL_WIDTH)
                 }
               }
-              Text
             }
           }
-          id
         }
       }
     }
   `);
   return query;
 };
+
+const getWindowDimensions = () => {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height
+  };
+}
+
+export const useWindowDimensions = () => {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowDimensions;
+}
 
 export default useBitwayPage;
