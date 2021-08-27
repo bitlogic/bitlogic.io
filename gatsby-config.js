7239f7940092ad6e78c20a66ade1ca093f7184d6
Benjamin Cañas <benjamin.canas@bitlogic.io>
2021-08-27 09:57:44 -0300

    bitway/fix de bugs

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,8 +17,8 @@ module.exports = {
     {
       resolve: "gatsby-source-strapi",
       options: {
-        // apiURL: `http://lb-bitlogic-strapi-dev-48805770.sa-east-1.elb.amazonaws.com:1337`,
-        apiURL: process.env.STRAPI_URL,
+        apiURL: `http://lb-bitlogic-strapi-dev-48805770.sa-east-1.elb.amazonaws.com:1337`,
+        // apiURL: process.env.STRAPI_URL,
         queryLimit: 1000,
         contentTypes: ["banners", "services", "edTeches", "ed-tech-submodules", "article", "blog-category",],
         singleTypes: [`ed-tech-page`, `global-seo`, `services-page`, `home`, `blog-page`, `bitway-page`, `contact-page`],
