666b90685068a796e42d1ffa25de465c1f40c100
Drina_Rincon <drina.rincon@bitlogic.io>
2022-01-24 14:35:50 -0300

    mostrar componente banner en gatsby

--- src/templates/Page.js
+++ src/templates/Page.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import { graphql } from "gatsby";
+import React from 'react'
+import { graphql, Link } from "gatsby"
+//import { getImage, GatsbyImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import { Seo } from "../components/index.js"
+import './Page.scss'
 
 const Page = ({ data }) => {
-    const { title } = data?.allStrapiPage?.nodes[0]
+  const { title, sections } = data?.allStrapiPage?.nodes[0]
 
-    return (
-        <Layout>
-            <Seo title={title} />
-            <h1>Estamos en PAGE</h1>
-        </Layout>
-    );
+  return (
+    <Layout>
+      <Seo title={title} />
+      {sections?.map((section, idx) => (
+        <div className="content__container">
+          <div className="content__img">
+            <img src='http://localhost:1337/uploads/Whats_App_Image_2020_01_22_at_11_23_1_dae1b32eaa.png'  key={idx}/>
+          </div>
+          <div className="content__description">
+            <h5>{section?.title}</h5>
+            <Link to={`/${section?.link?.pathTo}`} className="content__link">
+              {section?.link?.name}
+            </Link>
+          </div>
+        </div>
+      ))}
+
+    </Layout>
+  );
 }
 
 
@@ -21,8 +36,10 @@ export const query = graphql`
         nodes {
           title
           slug
+          sections
         }
     }
   }
 `
 export default Page
+
