575b7a968caa763c52f7a930a0b070f24564d050
benjacanas <benjamin.canas@bitlogic.io>
2022-02-21 10:21:12 -0300

    video background añadido

--- src/components/videoBackground/VideoBackground.js
+++ src/components/videoBackground/VideoBackground.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useEffect, useRef } from "react"
+import { useState } from "react"
 import "./videoBackground.scss"
-const VideoBackground = ({data}) => {
+const VideoBackground = ({ data: { video, description, button } }) => {
+  const [isVideoPause, setIsVideoPause] = useState(false)
+  const videoRef = useRef(null)
+
+  const pausePlay = () => {
+    if (isVideoPause) {
+        videoRef.current.play()
+    } else {
+        videoRef.current.pause()
+    }
+    setIsVideoPause(prev => !prev)
+  }
+
+
+  useEffect(() => {
+
+    const isVideoPauseLocal = typeof window !== 'undefined' ? localStorage.getItem('videoPaused') : undefined
     
+    if (isVideoPauseLocal === "true") {
+        console.log(isVideoPauseLocal)
+        console.log("video pausado localmente")
+        videoRef.current.pause()
+        setIsVideoPause(isVideoPauseLocal)
+    }
+  }, [])
+  
+  useEffect(() => {
+    localStorage.setItem('videoPaused', isVideoPause)
+  }, [isVideoPause])
+
+
   return (
-    <div className='videoBackground' >VideoBackground</div>
+    <section className="videoBackground">
+      <video
+        ref={videoRef}
+        muted
+        autoPlay
+        loop
+        src={video.url}
+        type="video/mp4"
+        controls={false}
+        onClick={pausePlay}
+      />
+      <div className="videoBackground-card" >
+        <p>{description}</p>
+        <a href={button.url} target="_blank" rel="noreferrer">
+          <button >{button.content}</button>
+        </a>
+      </div>
+    </section>
   )
 }
 
