2f7128b423a944d7b125136bdf0a50748944c169
Drina_Rincon <drina.rincon@bitlogic.io>
2022-02-21 11:02:33 -0300

    Agregar StrapiHome al esquema

--- src/schema/schema.js
+++ src/schema/schema.js
@@ -1,4 +1,40 @@
 const schema = `
+
+type StrapiHome implements Node {
+  body: JSON
+  published_at(
+    formatString: String
+    fromNow: Boolean
+    difference: String
+    locale: String
+  ): Date
+  created_at(
+    formatString: String
+    fromNow: Boolean
+    difference: String
+    locale: String
+  ): Date
+  updated_at(
+    formatString: String
+    fromNow: Boolean
+    difference: String
+    locale: String
+  ): Date
+  pageMetadata: StrapiHomePageMetadata
+  strapiId: Int
+  id: ID!
+  parent: Node
+  children: [Node!]!
+  internal: Internal!
+}
+
+type StrapiHomePageMetadata {
+  id: Int
+  pageTitle: String
+  pageDescription: String
+  pageKeywords: String
+}
+
 type StrapiServicesPage implements Node {
   id: ID!
   parent: Node
