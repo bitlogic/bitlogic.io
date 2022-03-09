613572fbb20d81f0d46a3290c8d59b9c4c1a892c
Drina_Rincon <drina.rincon@bitlogic.io>
2022-03-09 12:37:03 -0300

    Agregar componente bannerlist a landing

--- src/templates/LandingPage.js
+++ src/templates/LandingPage.js
@@ -5,18 +5,19 @@ import { Seo } from "../components/index.js"
 
 // componentes del body
 import Hero from "../components/Hero/Hero"
+import BannerList from '../components/BannerList/BannerLis';
 
 const BlogDetail = ({ data }) => {
   const pageData = data?.allStrapiLandingPage?.nodes[0]
 
   const bodyComponents = {
     "home.hero": data => <Hero data={data} />,
-    "components.banner-list": data => <p>banner list</p>,
+    "components.banner-list": data => <BannerList data={data} />,
     "components.selected-grid": data => <p>selected grid</p>,
   }
 
   return (
-    <Layout options={{hasHeader: false}} >
+    <Layout options={{ hasHeader: false }} >
       <Seo title={pageData.name} />
       {/* Dynamic zone */}
       {pageData.body.map(component =>
