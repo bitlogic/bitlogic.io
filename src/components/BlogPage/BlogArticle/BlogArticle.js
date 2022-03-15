c0d75bc2896e7fc154755132d2d45f03fd7c1c87
Drina_Rincon <drina.rincon@bitlogic.io>
2022-03-15 10:20:51 -0300

    Cambiar Link de Ver Más a un button

--- src/components/BlogPage/BlogArticle/BlogArticle.js
+++ src/components/BlogPage/BlogArticle/BlogArticle.js
@@ -20,7 +20,9 @@ const BlogArticle = ({ title, summary, image, slug, text }) => {
           {/* <ReactMarkdown source={`${summary} ...`} /> */}
         </div>
         <div className="article__link">
-          <Link to={slug}>{text}</Link>
+          <Link to={slug}>
+            <button className="px-4 py-2">{text}</button>
+          </Link>
         </div>
       </div>
     </div>
