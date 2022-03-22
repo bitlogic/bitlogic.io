01acf7694abf2a59873686d91fb7b11bc830c339
benjacanas <benjamin.canas@bitlogic.io>
2022-03-22 13:22:33 -0300

    esquemas reorganizados y componentes viejos eliminados

--- src/components/index.js
+++ src/components/index.js
@@ -1,41 +1,8 @@
-import ServicesSection from "./ServicesPage/ServicesSection"
-import ServiceCard from "./ServicesPage/ServiceCard"
-import Cards from "./Cards/Cards"
-import SubModuleItems from "./SubmoduleItems/SubmoduleItems"
-import SubModulesEdTech from "./SubModulesEdTech/SubModulesEdTech"
-import Title from "./Title/Title"
 import Seo from "./Seo/Seo"
-import EdTechCards from "./EdTechCards/EdTechCards"
-import ServiceCards from "./ServiceCards/ServiceCards"
-import CustomSection from "./CustomSection/CustomSection"
-import BannerBgImage from "./Banners/BannerBgImage"
-import BannerLogo from "./Banners/BannerLogo"
-import BannerISO from "./Banners/BannerISO"
-import BannerClientes from "./Banners/BannerClientes"
-import BannerTop from "./Banners/BannerTop"
-import BannerActionCall from "./Banners/BannerActionCall"
-import PartnersSection from "./PartnersSection/PartnersSection"
-import JobsPage from "./JobsPage/JobsPage"
 import CasesSection from "./CasesSection/CasesSection"
 
 export {
-  ServicesSection,
-  ServiceCard,
-  Cards,
   CasesSection,
-  SubModuleItems,
-  SubModulesEdTech,
-  Title,
   Seo,
-  EdTechCards,
-  ServiceCards,
-  CustomSection,
-  BannerBgImage,
-  BannerLogo,
-  BannerISO,
-  BannerClientes,
-  BannerTop,
-  BannerActionCall,
-  PartnersSection,
-  JobsPage,
+ 
 }
--- src/pages/jobs.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import {JobsPage} from "../components"
-
-const Jobs = () => <JobsPage />
-
-export default Jobs
