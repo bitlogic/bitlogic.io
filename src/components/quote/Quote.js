cbd1b2b9256290c264c484a39e3d227f43fa858e
Drina_Rincon <drina.rincon@bitlogic.io>
2022-04-06 13:54:09 -0300

    Modificar quote para que acepte video youtube

--- src/components/quote/Quote.js
+++ src/components/quote/Quote.js
@@ -1,61 +1,49 @@
 import "./quote.scss"
 import MarkdownView from "react-showdown"
-import React, { useEffect, useRef, useState } from "react"
+import React from "react"
 
 const Quote = ({
-  data: { description, title, variant, profile, profileDescription, button, image, strapi_component, id },
+  data: { description, title, variant, profile, profileDescription, button, image, videoUrl, strapi_component, id },
 }) => {
-  const [isVideoPause, setIsVideoPause] = useState(false)
-  const videoRef = useRef(null)
+  const url = videoUrl.replace("watch?v=", "embed/")
+  let code = url.substring(url.lastIndexOf("/") + 1, url.length)
+  const codeIndex = code.indexOf("?")
 
-  const pausePlay = () => {
-    if (isVideoPause) {
-      videoRef.current.play()
-    } else {
-      videoRef.current.pause()
-    }
-    setIsVideoPause(prev => !prev)
+  if (codeIndex !== -1) {
+    code = code.substring(0, code.indexOf("?"))
   }
-
-  useEffect(() => {
-    const isVideoPauseLocal =
-      typeof window !== "undefined"
-        ? localStorage.getItem("videoPaused")
-        : undefined
-
-    if (isVideoPauseLocal === "true") {
-      videoRef.current.pause()
-      setIsVideoPause(isVideoPauseLocal)
-    }
-  }, [])
-
-  useEffect(() => {
-    localStorage.setItem("videoPaused", isVideoPause)
-  }, [isVideoPause])
 
   return (
     <div className="container my-3 mb-lg-5" id={strapi_component + "-" + id}>
       <section className={`quote variant-${variant}`}>
         {image && (
           <div className="quote-body">
-            {(/video/gm.test(image.mime)) &&
-              <video
-                ref={videoRef}
-                muted
-                autoPlay
-                loop
-                src={image.url}
-                type="video/mp4"
-                onClick={pausePlay}
-                controls={false}
-              />}
-            {(/image/gm.test(image.mime)) && (
-              <img
-                src={image.url}
-                alt={image.name}
-              />
-            )
-            }
+            <img
+              src="http://localhost:1337/uploads/quote1_df2d40290b.png"
+              alt={image.name}
+            />
+          </div>
+        )}
+
+        {videoUrl && (
+          <div className="quote-body">
+            <iframe
+              loading="lazy"
+              type="text/html"
+              src={url}
+              frameBorder="0"
+              allowFullScreen
+              title="benefits_video"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              srcDoc={`<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;
+                            width:100%;top:0;bottom:0;max-height: 500px}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;margin:auto;text-shadow:0 0 0.5em black}</style>
+                            <a href=${url}>
+                            <img src=https://img.youtube.com/vi/${code}/hqdefault.jpg alt='Video'>
+                            <span>▶</span></a>`}
+              webkitallowfullscreen="true"
+              mozallowfullscreen="true"
+            ></iframe>
+
           </div>
         )}
 
@@ -64,7 +52,7 @@ const Quote = ({
           <p className="quote-person-text">{description}</p>
 
           {profile && (
-            <div className="quote-profile make-it-fast my-3 d-flex gap-3 justify-content-between">
+            <div className="quote-profile make-it-fast my-3 my-md-2 my-xl-4 d-flex gap-3 justify-content-between">
               <img
                 src={"http://localhost:1337/uploads/profile_4f038f59f5.png"}
                 alt=""
@@ -90,6 +78,7 @@ Quote.defaultProps = {
   title: "",
   variant: "",
   profileDescription: "",
+  videoUrl: "",
   button: {},
   profile: {},
   image: {},
