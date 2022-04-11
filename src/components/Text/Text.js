c922514270800a1e670a896f4589652d486b1acd
Drina_Rincon <drina.rincon@bitlogic.io>
2022-04-11 14:58:23 -0300

    Agregar bgImage al componente Text

--- src/components/Text/Text.js
+++ src/components/Text/Text.js
@@ -1,20 +1,35 @@
-import React from 'react';
-import MarkdownView from "react-showdown";
-import './Text.scss';
+import React from "react"
+import MarkdownView from "react-showdown"
+import "./Text.scss"
 
 export default function Text({ data }) {
     console.log(data)
-    const title = data?.title;
-    const description = data?.text;
+    const title = data?.title
+    const description = data?.text
+    const bgImage = data?.backgroundImage[0].url
+    console.log(bgImage)
 
     return (
-        <div className="text container my-3 d-flex flex-column flex-lg-row">
-
-            {title && (
-                <h2 className="title me-3 mt-5 p-lg-2">{title}</h2>
-            )}
-            {description && (
-                <MarkdownView markdown={description} className="description px-lg-5" />
+        <div className="container-text">
+            {title !== "" && title !== undefined && title !== null ? (
+                <div className="text d-flex flex-column flex-md-row">
+                    <div
+                        className="title"
+                        style={{
+                            backgroundImage: `url('http://localhost:1337/uploads/trama_gris_arriba_4dab8e35c4.png')`,
+                        }}
+                    >
+                        <h2 className="titleText pt-5 ps-4 pe-md-3">{title}</h2>
+                    </div>
+                    <MarkdownView
+                        markdown={description}
+                        className="description ps-4 px-lg-5"
+                    />
+                </div>
+            ) : (
+                <MarkdownView markdown={description} className="px-lg-5" id="descriptionText" style={{
+                    backgroundImage: `url('http://localhost:1337/uploads/trama_gris_arriba_4dab8e35c4.png')`,
+                }} />
             )}
         </div>
     )
