127d771e75490297a00342fa2e11b44029d5ef1c
benjacanas <benjamin.canas@bitlogic.io>
2022-04-12 12:07:50 -0300

    banner list cambios

--- src/components/BannerList/BannerLis.js
+++ src/components/BannerList/BannerLis.js
@@ -1,10 +1,11 @@
+import { Link } from "gatsby"
 import React from "react"
 import "./Banner.scss"
 
 export default function BannerLis({ data }) {
   const title = data?.title
   const image = data?.Card[0]?.icon?.url
-
+  console.log(data)
   const cards = data?.Card.map(item => {
     return (
       <div className="card_item d-flex mb-4">
@@ -12,7 +13,9 @@ export default function BannerLis({ data }) {
           <img class="mx-auto d-block" src={image} placeholder="blurred" />
         </div>
         <div className="card_item col-9 ps-2 pe-2">
-          <h4>{item.title}</h4>
+          <Link to={item.landing_page ? "/" + item.landing_page?.slug : null}>
+            <h4>{item.title}</h4>
+          </Link>
           <p>{item.description}</p>
         </div>
       </div>
@@ -20,15 +23,17 @@ export default function BannerLis({ data }) {
   })
 
   return (
-    <div
-      className="container my-3 py-3"
-      id={data.strapi_component + "-" + data.id}
-    >
+    <div className="container py-5" id={data.strapi_component + "-" + data.id}>
       <div className="bannerList d-md-flex flex-row-reverse">
+        <div className="bannerList__cards col-md-6 col-xl-6 pe-5">{cards}</div>
         <h1 className="bannerList__title col-md-6 col-xl-6 align-self-center ps-4 mb-4">
           {title}
+          {data.contactForm && (
+            <button>
+              <a href={"#" + data.concactFormAnchor}>Contactanos</a>
+            </button>
+          )}
         </h1>
-        <div className="bannerList__cards col-md-6 col-xl-6 pe-5">{cards}</div>
       </div>
     </div>
   )
--- src/components/Text/Text.js
+++ src/components/Text/Text.js
@@ -3,32 +3,37 @@ import MarkdownView from "react-showdown"
 import "./Text.scss"
 
 export default function Text({ data }) {
-    const title = data?.title
-    const description = data?.text
-    const bgImage = data?.backgroundImage?.url
+  const title = data?.title
+  const description = data?.text
+  const bgImage = data?.backgroundImage?.url
 
-    return (
-        <div className="container-text">
-            {title !== "" && title !== undefined && title !== null ? (
-                <div className="text d-flex flex-column flex-md-row">
-                    <div
-                        className="title"
-                        style={{
-                            backgroundImage: `url(${bgImage})`,
-                        }}
-                    >
-                        <h2 className="titleText pt-5 ps-4 pe-md-3">{title}</h2>
-                    </div>
-                    <MarkdownView
-                        markdown={description}
-                        className="description ps-4 px-lg-5"
-                    />
-                </div>
-            ) : (
-                <MarkdownView markdown={description} className="px-lg-5" id="descriptionText" style={{
-                    backgroundImage: `url(${bgImage})`,
-                }} />
-            )}
+  return (
+    <div className="container-text">
+      {title !== "" && title !== undefined && title !== null ? (
+        <div className="text d-flex flex-column flex-md-row">
+          <div
+            className="title"
+            style={{
+              backgroundImage: `url(${bgImage})`,
+            }}
+          >
+            <h2 className="titleText pt-5 ps-4 pe-md-3">{title}</h2>
+          </div>
+          <MarkdownView
+            markdown={description}
+            className="description ps-4 px-lg-5"
+          />
         </div>
-    )
+      ) : (
+        <MarkdownView
+          markdown={description}
+          className="px-lg-5"
+          id="descriptionText"
+          style={{
+            backgroundImage: `url(${bgImage})`,
+          }}
+        />
+      )}
+    </div>
+  )
 }
