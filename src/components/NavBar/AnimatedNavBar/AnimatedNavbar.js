affb4cc5205ec024b2f872bb12726f7d01a961f6
benjacanas <benjamin.canas@bitlogic.io>
2022-04-19 11:50:27 -0300

    navbar fixes

--- src/components/NavBar/AnimatedNavBar/AnimatedNavbar.js
+++ src/components/NavBar/AnimatedNavBar/AnimatedNavbar.js
@@ -16,11 +16,20 @@ const getComponentTitle = component => {
     "home.video-background": () => component.title,
     "home.dual-section": () =>
       component.dualSectionPart.map(section => section.title).join(" - "),
+    "components.text": () => false,
+    "banners.banner-head": () => component.title,
+    "components.logos-section": () => component.title,
+    "components.form": () => component.title,
+    "components.banner": () => component.title,
   }
-  return (
+  console.log(
     (titleReference[component.strapi_component] &&
       titleReference[component.strapi_component]()) ||
-    "Titulo no definido"
+      component.strapi_component
+  )
+  return (
+    titleReference[component.strapi_component] &&
+    titleReference[component.strapi_component]()
   )
 }
 
@@ -43,7 +52,7 @@ const AnimatedNavbar = ({ landingComponents, navbarItems = [], duration }) => {
                 sections={landingComponents
                   .find(landing => landing.name === navItem.landing.name)
                   .body.map(component => ({
-                    name: getComponentTitle(component),
+                    name: getComponentTitle(component) || "",
                     id: component.strapi_component + "-" + component.id,
                     slug: navItem.landing.slug,
                   }))}
