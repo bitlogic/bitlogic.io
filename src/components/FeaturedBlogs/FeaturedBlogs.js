bae9b1e6173b40aef2df1b0b0b47bc8c00836ea8
Yuliya Sharapa <yuliya.sharapa@bitlogic.io>
2022-04-22 10:54:47 -0300

    center title and summary in featured blogs

--- src/components/FeaturedBlogs/FeaturedBlogs.js
+++ src/components/FeaturedBlogs/FeaturedBlogs.js
@@ -7,7 +7,7 @@ const FeaturedBlogs = ({ data }) => {
   return (
     <div className="container featured" id={data.strapi_component + "-" + data.id}>
       <h2>{data.title}</h2>
-      <h6 className="px-5">{data.subtitle}</h6>
+      <h6 className="px-md-3">{data.subtitle}</h6>
 
       <div className="featured-blogs">
         {data.articles.slice(0, 3).map((item, idx) => (
