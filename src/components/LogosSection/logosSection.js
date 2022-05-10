68b4484a5763792c58b6d762e6731ca2f23bd802
Drina_Rincon <drina.rincon@bitlogic.io>
2022-05-10 08:32:19 -0300

    Agregar flechas en mobile y breakpoint entre mobile y tablet

--- src/components/LogosSection/logosSection.js
+++ src/components/LogosSection/logosSection.js
@@ -31,11 +31,15 @@ const LogosSection = ({ data }) => {
       items: 5
     },
     tablet: {
-      breakpoint: { max: 1024, min: 464 },
+      breakpoint: { max: 1024, min: 768 },
       items: 3
     },
+    mobileTablet: {
+      breakpoint: { max: 767, min: 577 },
+      items: 2
+    },
     mobile: {
-      breakpoint: { max: 464, min: 0 },
+      breakpoint: { max: 576, min: 0 },
       items: 1
     }
   };
@@ -59,11 +63,10 @@ const LogosSection = ({ data }) => {
 
       <Carousel
         responsive={responsive}
-        //autoPlay={true}
+        autoPlay={true}
         autoPlaySpeed={3000}
         rewind={true}
         infinite={true}
-        removeArrowOnDeviceType={'mobile'}
         containerClass={'containerCarrusel'}
         customRightArrow={<CustomRightArrow />}
         customLeftArrow={<CustomLeftArrow />}
