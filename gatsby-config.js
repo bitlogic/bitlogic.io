d309025919b6dc92b7460b6aff49351f886324c8
GonzalezMaxi <maximiliano.gonzalez@bitlogic.io>
2024-05-22 12:48:30 -0300

    SBIT-298: integrate gatsby-plugin-sitemap & gatsby-plugin-tobots-txt

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,10 +3,73 @@ module.exports = {
     title: `Bitlogic`,
     description: `Bitlogic Web es una empresa dedicada al diseño, ingeniería y desarrollo ágil de productos de software, especializada en la transformación digital de instituciones educativas .`,
     author: `Bitlogic.io`,
-    siteUrl: process.env.SITE_URL,    
+    siteUrl: process.env.SITE_URL,
+    // siteUrl: 'https://bitlogic.io',
   },
   plugins: [
-    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        output: '/',
+        query: `
+        {
+          allSitePage {
+            nodes {
+              path
+              pageContext
+            }
+          }
+          strapiBlogPage {
+            updated_at
+          }
+          strapiHome {
+            updated_at
+          }
+        }
+        `,
+        resolveSiteUrl: () => process.env.SITE_URL,
+        resolvePages: ({
+          allSitePage: { nodes: allPages },
+          strapiBlogPage: blogPage,
+          strapiHome: homePage
+        }) => {
+          const singlePages = [
+            {
+              path: '/',
+              lastmod: homePage.updated_at
+            },
+            {
+              path: '/blog',
+              lastmod: blogPage.updated_at
+            }
+          ];
+
+          return allPages.map(page => {
+            if (page.path === '/') return singlePages[0]
+            else if (page.path === '/blog/') return singlePages[1]
+
+            return {
+              path: page.path,
+              lastmod: page?.pageContext?.lastmod
+            }
+          })
+        },
+        serialize: ({ path, lastmod }) => {
+          return {
+            url: path,
+            lastmod: lastmod,
+          }
+        },
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        host: process.env.SITE_URL,
+        sitemap: `${process.env.SITE_URL}/sitemap-index.xml`,
+        policy: [{ userAgent: '*', allow: '/' }]
+      }
+    },
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
@@ -32,6 +95,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-canonical-urls`,
       options: {
+        // siteUrl: 'https://bitlogic.io',
         siteUrl: process.env.SITE_URL,
       },
     },
@@ -41,6 +105,7 @@ module.exports = {
         // apiURL: `http://lb-bitlogic-strapi-dev-48805770.sa-east-1.elb.amazonaws.com:1337`,
         // apiURL: `https://strapi.bitlogic.io`,
         apiURL: process.env.STRAPI_URL,
+        // apiURL: 'http://127.0.0.1:1337',
         queryLimit: 1000,
         collectionTypes: [
           `article`,
