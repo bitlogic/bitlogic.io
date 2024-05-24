f661f7ce89f9f75baae77e9a19f04c4f394f998e
GonzalezMaxi <maximiliano.gonzalez@bitlogic.io>
2024-05-24 09:51:48 -0300

    SBIT-297: Create CustomSection & remove deprecated components

---
+++ src/components/CustomSection/CustomSection.js
@@ -0,0 +1,156 @@
+import React from "react";
+
+/* Imports sections */
+// import BlogContainer from '../BlogPage/BlogContainer'
+import VideoBackground from "../videoBackground/VideoBackground";
+import Hero from '../Hero/Hero';
+import {
+  AnimatedTransitionContinous,
+  Banner,
+  BannerList,
+  CasesSection,
+  CasesList,
+  Catsone,
+  DualSection,
+  OneSection,
+  ExpandGrid,
+  FeaturedBlogs,
+  // PipedriveForm,
+  LogosSection,
+  Professionals,
+  Quote,
+  Text,
+  Form
+} from '../';
+
+const CustomSection = ({ sections, location }) => {
+
+  const sectionResult = sections.map((section) => {
+    if (section?.strapi_component === null ||
+      section?.strapi_component === undefined) {
+      return <></>
+    }
+
+    if (section.strapi_component === 'home.transition') {
+      return <AnimatedTransitionContinous data={section}
+        key={section.strapi_component + section.id}
+      />
+    }
+
+    if (section.strapi_component === 'components.banner') {
+      return <Banner data={section}
+        key={section.strapi_component + section.id}
+      />
+    }
+
+    if (section.strapi_component === 'components.banner-list') {
+      return <BannerList data={section}
+        key={section.strapi_component + section.id}
+      />
+    }
+
+    if (section.strapi_component === 'components.cases-section'
+      && !section?.allCases) {
+      return <CasesSection data={section}
+        key={section.strapi_component + section.id}
+      />
+    }
+
+    if (section.strapi_component === 'components.cases-section'
+      && section?.allCases) {
+      return <CasesList data={section}
+        key={section.strapi_component + section.id}
+      />
+    }
+
+    if (section.strapi_component === 'scripts.catsone') {
+      return <Catsone data={section}
+        key={section.strapi_component + section.id}
+      />
+    }
+    if (section.strapi_component === 'home.dual-section') {
+      const sectionComponent = section?.dualSectionPart.length === 1
+        ? <OneSection data={section}
+          key={section.strapi_component + section.id}
+        />
+        : <DualSection data={section}
+          key={section.strapi_component + section.id}
+        />
+
+      return sectionComponent
+    }
+
+    if (section.strapi_component === 'components.selected-grid') {
+      return <ExpandGrid data={section}
+        key={section.strapi_component + section.id}
+      />
+    }
+
+    if (section.strapi_component === 'components.featured-blogs') {
+      return <FeaturedBlogs data={section}
+        key={section.strapi_component + section.id}
+      />
+    }
+
+    // if (section.strapi_component === 'components.pipedrive-form') {
+    //   return <PipedriveForm data={section}
+    //     key={section.strapi_component + section.id}
+    //   />
+    // }
+
+    if (section.strapi_component === 'components.form') {
+      return (
+        <Form key={section.strapi_component + section.id}
+          location={location}
+          data={section}
+        />
+      )
+    }
+
+    if (section.strapi_component === 'components.logos-section') {
+      return <LogosSection data={section}
+        key={section.strapi_component + section.id}
+      />
+    }
+
+    if (section.strapi_component === 'components.professionals-section') {
+      return <Professionals data={section}
+        key={section.strapi_component + section.id}
+      />
+    }
+
+    if (section.strapi_component === 'home.quote') {
+      return <Quote data={section}
+        key={section.strapi_component + section.id}
+      />
+    }
+
+    if (section.strapi_component === 'components.text') {
+      return <Text data={section}
+        key={section.strapi_component + section.id}
+      />
+    }
+
+    if (section.strapi_component === 'home.video-background') {
+      return <VideoBackground data={section}
+        key={section.strapi_component + section.id}
+      />
+    }
+
+    if (section.strapi_component === 'home.hero') {
+      return <Hero data={section}
+        key={section.strapi_component + section.id}
+      />
+    }
+
+    return <></>
+  })
+
+  return (
+    <>
+      {sectionResult}
+    </>
+  )
+}
+
+export default CustomSection;
--- src/templates/LandingPage.js
+++ src/templates/LandingPage.js
@@ -1,148 +1,21 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
-import BlogContainer from '../components/BlogPage/BlogContainer'
 
-// componentes del body
 import {
   Seo,
-  CasesSection,
-  CasesList,
-  LogosSection,
-  Banner,
-  Form,
-  BannerList,
-  ExpandGrid,
-  Catsone,
-  Text,
-  AnimatedTransitionContinous,
-  DualSection,
-  OneSection,
-  Quote,
-  Professionals,
-  FeaturedBlogs,
+  CustomSection
 } from "../components"
-
-import VideoBackground from "../components/videoBackground/VideoBackground"
 
 const LandingPage = ({ data, location }) => {
   const pageData = data?.allStrapiLandingPage?.nodes[0]
-  const content = pageData.body.map((component, idx) => {
-    const bannerList =
-      component.strapi_component === "components.banner-list" ? (
-        <BannerList data={component} />
-      ) : null
-
-    const expandGrid =
-      component.strapi_component === "components.selected-grid" ? (
-        <ExpandGrid data={component} />
-      ) : null
-
-    const casesSection =
-      component.strapi_component === "components.cases-section" &&
-        !component.allCases ? (
-        <CasesSection data={component} />
-      ) : null
-
-    const casesList =
-      component.strapi_component === "components.cases-section" &&
-        component.allCases ? (
-        <CasesList />
-      ) : null
-
-    const catsone =
-      component.strapi_component === "scripts.catsone" ? (
-        <Catsone data={component} />
-      ) : null
-
-    const text =
-      component.strapi_component === "components.text" ? (
-        <Text data={component} />
-      ) : null
-
-    const logosSection =
-      component.strapi_component === "components.logos-section" ? (
-        <LogosSection data={component} />
-      ) : null
-
-    const form =
-      component.strapi_component === "components.form" ? (
-        <Form data={component} location={location} />
-      ) : null
-
-    const banner =
-      component.strapi_component === "components.banner" ? (
-        <Banner data={component} />
-      ) : null
-
-    const quote =
-      component.strapi_component === "home.quote" ? (
-        <Quote data={component} />
-      ) : null
-
-    const dualSection =
-      component.strapi_component === "home.dual-section" &&
-      (component.dualSectionPart.length === 1 ? (
-        <OneSection data={component} />
-      ) : (
-        <DualSection data={component} />
-      ))
-
-    const animatedTransition =
-      component.strapi_component === "home.transition" ? (
-        <AnimatedTransitionContinous data={component} />
-      ) : null
-
-    const videoBackground =
-      component.strapi_component === "home.video-background" ? (
-        <VideoBackground data={component} />
-      ) : null
-
-    const professionals =
-      component.strapi_component === "components.professionals-section" ? (
-        <Professionals data={component} />
-      ) : null
-
-    const featuredBlogs =
-      component.strapi_component === "components.featured-blogs" && !component.allBlog ? (
-        <FeaturedBlogs data={component} />
-      ) : null
-
-    const blogContainer =
-      component.strapi_component === "components.featured-blogs" &&
-        component.allBlog ? (
-        <BlogContainer />
-      ) : null
-
-
-    return (
-      <div key={idx}>
-        <>
-          {banner}
-          {bannerList}
-          {expandGrid}
-          {casesSection}
-          {casesList}
-          {catsone}
-          {text}
-          {logosSection}
-          {form}
-          {videoBackground}
-          {quote}
-          {dualSection}
-          {animatedTransition}
-          {professionals}
-          {featuredBlogs}
-          {blogContainer}
-        </>
-      </div>
-    )
-  })
 
   return (
     <Layout location={location} options={{ hasHeader: true }}>
       <Seo title={pageData.name} />
-      {content}
+      {pageData?.body?.length > 0 && (
+        <CustomSection sections={pageData.body} />
+      )}
     </Layout>
   )
 }
