8955705053ea6efe2b00e27880e3e3ea09fdc9f5
Rocío Llanes <rocio.llanes@bitlogic.io>
2024-05-29 16:06:48 -0300

    fix on constants

--- src/components/Footer/Subscription/subscription.js
+++ src/components/Footer/Subscription/subscription.js
@@ -6,8 +6,8 @@ import "./subscription.scss"
 export default function Subscription() {
   const data = useFooter()
   const dataSubscription = data?.allStrapiLayout?.nodes[0]?.footer?.subscription
-  const subscriptionUrl = data?.allStrapiLayout?.nodes[0]?.footer?.subscription?.url
-  const subscriptionLanding = data?.allStrapiLayout?.nodes[0]?.footer?.subscription?.landing_page?.slug
+  const subscriptionUrl = dataSubscription?.url
+  const subscriptionLanding = dataSubscription?.landing_page?.slug
 
   const isExternalLink = subscriptionUrl?.startsWith('http')
 
