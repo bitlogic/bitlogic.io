bf6ec794f3e5876ff8f28b3ff38dee90741ce9fa
Juana <juana.balduini@bitlogic.io>
2025-05-15 11:48:33 -0300

    Se deshacen los cambios de react-helmet

--- gatsby-config.js
+++ gatsby-config.js
@@ -103,8 +103,8 @@ module.exports = {
       options: {
         // apiURL: `http://lb-bitlogic-strapi-dev-48805770.sa-east-1.elb.amazonaws.com:1337`,
         // apiURL: `https://strapi.bitlogic.io`,
-        //apiURL: process.env.STRAPI_URL,
-         apiURL: "http://127.0.0.1:1337",
+        apiURL: process.env.STRAPI_URL,
+        // apiURL: "http://127.0.0.1:1337",
         queryLimit: 1000,
         collectionTypes: [
           `article`,
